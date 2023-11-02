45935475234d970ce7671af02d6197f6ba012db3
LittleArmy <farhan.salim2210@gmail.com>
2023-11-02 22:34:42 +0700

    02 November API store

--- application/restapi/controller/controller.js
+++ application/restapi/controller/controller.js
@@ -127,6 +127,113 @@ exports.deleteproductbrand = function(req,res){
     });
 };
 
+exports.showallstore = function(req,res){
+    connection.query('select * from store', function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.showstore = function(req,res){
+    let id = req.params.id;
+    console.log(id);
+
+    connection.query('select * from store where store_id = ?',[id], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.showstoreacc = function(req,res){
+    let id = req.params.id;
+    console.log(id);
+
+    connection.query('select * from store where account_id = ?',[id], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.showstorearea = function(req,res){
+    let id = req.params.id;
+    console.log(id);
+
+    connection.query('select * from store where area_id = ?',[id], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.showstorestatus = function(req,res){
+    let id = req.params.id;
+    console.log(id);
+
+    connection.query('select * from store where is_active = ?',[id], function(error,rows,fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.addstore = function(req,res){
+    var store_name = req.body.store_name;
+    var acc_id = req.body.account_id;
+    var area_id = req.body.area_id;
+    var is_active = req.body.is_active;
+
+    connection.query('INSERT INTO product_brand (store_name,account_id,area_id,is_active) VALUES(?)',[store_name,acc_id,area_id,is_active], function(error){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil Menambahkan Data!",res)
+        }
+    });
+};
+
+exports.updatestore = function(req,res){
+    var store_id = req.body.store_id;
+    var store_name = req.body.store_name;
+    var acc_id = req.body.account_id;
+    var area_id = req.body.area_id;
+    var is_active = req.body.is_active;
+
+    connection.query('UPDATE product_brand SET store_name = ?, account_id = ?, area_id = ?, is_active = ? where store_id = ?',[store_name,acc_id,area_id,is_active,store_id], function(error){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil Mengubah Data!",res)
+        }
+    });
+};
+
+exports.deletestore = function(req,res){
+    var store_id = req.body.store_id;
+
+    connection.query('DELETE from product_brand where store_id = ?',[store_id], function(error){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok("Berhasil Menghapus Data!",res)
+        }
+    });
+};
+
+
+
 // exports.showreportproduct = function(req,res){
 
 //     connection.query('SELECT * FROM `report_product` inner JOIN store on report_product.store_id = store.store_id',
