4579d833d77db18db27c1b2b94ea4e09eb52c424
LittleArmy <farhan.salim2210@gmail.com>
2023-12-19 02:02:36 +0700

    Add Tabs and Circular Progress

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.rtl.min.css" integrity="sha384-5/ZcxA7Dub2FNG09dHw8CHmPN7Fz6ASlweagj0nuXjmMyupgH9n9F5Hd926zsu3/" crossorigin="anonymous">
 
+
 <head>
 
     <meta charset="utf-8">
@@ -19,6 +20,8 @@
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <link href="css-circular-prog-bar.css" rel="stylesheet">
+    <link href="<?= base_url('assets/css/circle_progress.css') ?>" rel="stylesheet">
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
 </head>
